0514bbc25ecab5f1e86c73857f7c8b1b32cd9ef7
Kris Gray <kag56@cam.ac.uk>
2025-01-23 15:48:04 +0000

    Add Docker support with Dockerfile and .dockerignore; remove container.build.sh

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,9 @@
-FROM ubuntu/apache2
-LABEL Author="Kristian Gray <kag56@cam.ac.uk>"
-WORKDIR /var/www/
+FROM node:alpine
 
-RUN apt-get update -y && apt-get upgrade -y;
-RUN apt-get install -y curl;
-RUN curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash;
-RUN nvm install --lts && nvm use --lts;
+WORKDIR /app
+COPY . .
+RUN npm install
+RUN npm run build
+EXPOSE 3000
+
+CMD ["npm", "run", "start:prod"]
--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -1,9 +1,12 @@
 import { registerAs } from '@nestjs/config';
 
-export default registerAs('appConfig', () => ({
-  environment: process.env.NODE_ENV || 'production',
-  apiVersion: process.env.API_VERSION,
-  mailHost: process.env.MAIL_HOST,
-  smtpUsername: process.env.SMTP_USERNAME,
-  smtpPassword: process.env.SMTP_PASSWORD,
-}));
+export default registerAs('appConfig', () => {
+  console.log('ENV:', process.env);
+  return {
+    environment: process.env.NODE_ENV || 'production',
+    apiVersion: process.env.API_VERSION,
+    mailHost: process.env.MAIL_HOST,
+    smtpUsername: process.env.SMTP_USERNAME,
+    smtpPassword: process.env.SMTP_PASSWORD,
+  };
+});
