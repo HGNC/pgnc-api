75201ea98c8d80825009de06d44f6f8c3c3412e4
Kris Gray <kag56@cam.ac.uk>
2025-02-25 14:55:38 +0000

    Refactor environment variable handling and update database password key

--- src/config/environment.validation.ts
+++ src/config/environment.validation.ts
@@ -5,9 +5,9 @@ export default Joi.object({
   DB_PORT: Joi.number().default(5432),
   DB_HOST: Joi.string().required(),
   DB_USER: Joi.string().required(),
-  DB_PASS: Joi.string().required(),
+  DB_PASSWORD: Joi.string().required(),
   DB_NAME: Joi.string().required(),
-  DB_SYNC: Joi.boolean().required(),
+  DB_SYNC: Joi.string().required(),
   DB_AUTOLOAD_ENTITIES: Joi.boolean().required(),
   PROFILE_API_KEY: Joi.string().required(),
 
