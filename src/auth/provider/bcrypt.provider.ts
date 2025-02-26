c796d5ef5d202ca239752ed8fb7f3d007bfbd3c7
Kris Gray <kag56@cam.ac.uk>
2025-02-26 18:36:12 +0000

    Refactor BcryptProvider methods for consistent formatting and readability

--- src/auth/provider/bcrypt.provider.ts
+++ src/auth/provider/bcrypt.provider.ts
@@ -9,26 +9,26 @@ import * as bcrypt from 'bcrypt';
  */
 @Injectable()
 export class BcryptProvider implements HashingProvider {
-  /**
-   * Hashes the provided data. The data can be a string or a buffer.
-   * @param {(string | Buffer)} data The data to hash.
-   * @returns {Promise<string>} The hashed data.
-   */
-  public async hashPassword(data: string | Buffer): Promise<string> {
-    const salt = await bcrypt.genSalt();
-    return bcrypt.hash(data.toString(), salt);
-  }
+    /**
+     * Hashes the provided data. The data can be a string or a buffer.
+     * @param {(string | Buffer)} data The data to hash.
+     * @returns {Promise<string>} The hashed data.
+     */
+    public async hashPassword(data: string | Buffer): Promise<string> {
+        const salt = await bcrypt.genSalt();
+        return bcrypt.hash(data.toString(), salt);
+    }
 
-  /**
-   * Compares the provided data with the encrypted data.
-   * @param {(string | Buffer)} data The data to compare.
-   * @param {string} encrypted The encrypted data to compare against.
-   * @returns {Promise<boolean>} A promise that resolves to true if the data matches the encrypted data, false otherwise.
-   */
-  public async comparePassword(
-    data: string | Buffer,
-    encrypted: string,
-  ): Promise<boolean> {
-    return bcrypt.compare(data.toString(), encrypted);
-  }
+    /**
+     * Compares the provided data with the encrypted data.
+     * @param {(string | Buffer)} data The data to compare.
+     * @param {string} encrypted The encrypted data to compare against.
+     * @returns {Promise<boolean>} A promise that resolves to true if the data matches the encrypted data, false otherwise.
+     */
+    public async comparePassword(
+        data: string | Buffer,
+        encrypted: string,
+    ): Promise<boolean> {
+        return bcrypt.compare(data.toString(), encrypted);
+    }
 }
